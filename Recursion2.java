91c5d18c7300489cd060545e55250463bf78c0ec
Zoha afreen sabir Abdul <sheikhzoha927@gmail.com>
2026-08-24 21:02:23 +0530

    Added java program  based on recursion

---
+++ Recursion2.java
@@ -0,0 +1,18 @@
+//printing the reverse of a string ....abcd
+public class Recursion2 {
+        public static String revString(String str) {
+            if (str.length() == 1) {
+                return str;
+            }
+            char currChar = str.charAt(0);
+            String nextString = revString(str.substring(1));
+            return nextString + currChar;
+        }
+
+        public static void main(String args[]) {
+            String str = "abcd";
+            String reversed = revString(str);
+            System.out.println(reversed);
+        }
+    }
+
